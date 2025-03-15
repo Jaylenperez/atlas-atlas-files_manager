08f5f0471716aebea5d5c5603bd020ad0b2e5228
jaylenperez <jaylenestella24@gmail.com>
2025-03-15 16:20:33 -0500

    task 0 (unfinished) - set up redis client with basic get, set, and delete methods, encountering issue where isAlive returns undefined need to debug later

---
+++ utils/redis.js
@@ -0,0 +1,66 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    // Create Client
+    this.client = createClient();
+
+    // Success Message
+    this.client.on('connect', () => {
+      console.log('Connected Successfully to Redis');
+    });
+
+    // Error Message
+    this.client.on('error', (err) => {
+      console.error(`Redis Client Error: ${err}`);
+    });
+  }
+
+  // Returns true when Redis connection successful
+  isAlive() {
+    return this.client.isOpen;
+  }
+
+  // Returns Redis value stored for given key
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, value) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(value);
+        }
+      });
+    });
+  }
+
+  // Stores key value pairs and their expiration
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  // Removes a key value pair
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+}
+
+// Export a single instance of RedisClient
+const redisClient = new RedisClient();
+export default redisClient;
